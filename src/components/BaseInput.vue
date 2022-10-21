8c361b9d588d598ba0d3a213b871c942a0a2a06c
tamir3232 <tamirrasydi@gmail.com>
2022-10-21 14:52:32 +0700

    menambah baseinput yang sepertinya perlu

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "BaseInput",
+  emits: ["update:modelValue"],
+  props: {
+    name: String,
+    label: String,
+    modelValue: String,
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+};
+</script>
+
+<template>
+  <label :for="name">
+    <p>{{ label }}</p>
+    <input
+      :type="type"
+      :placeholder="label"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </label>
+</template>
